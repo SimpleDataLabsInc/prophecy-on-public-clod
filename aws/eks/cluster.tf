3e8bb97749fb3a6cb3c98189d435e71238ae3a9e
George Kuruvilla <georgekuruvillak@gmail.com>
2022-04-01 22:03:08 +0530

    Update cluster.tf

--- aws/eks/cluster.tf
+++ aws/eks/cluster.tf
@@ -81,8 +81,12 @@ resource "aws_eks_cluster" "cluster" {
 }
 
 ### OIDC config
+data "tls_certificate" "cluster" {
+  url = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
+}
+
 resource "aws_iam_openid_connect_provider" "cluster" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = []
-  url             = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = concat([data.tls_certificate.cluster.certificates.0.sha1_fingerprint], var.oidc_thumbprint_list)
+  url = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
 }
