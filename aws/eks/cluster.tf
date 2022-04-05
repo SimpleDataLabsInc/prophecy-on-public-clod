07ae2e14f3d93f2cbbbe671103a8ce3abc8a94ea
George Kuruvilla <georgekuruvillak@gmail.com>
2022-04-05 12:53:32 +0530

    Update cluster.tf with oidc configuration

--- aws/eks/cluster.tf
+++ aws/eks/cluster.tf
@@ -79,3 +79,14 @@ resource "aws_eks_cluster" "cluster" {
     aws_iam_role_policy_attachment.cluster-AmazonEKSServicePolicy,
   ]
 }
+
+### OIDC config
+data "tls_certificate" "cluster" {
+  url = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
+}
+
+resource "aws_iam_openid_connect_provider" "cluster" {
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [join("", data.tls_certificate.cluster.*.certificates.0.sha1_fingerprint)]
+  url = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
+}
